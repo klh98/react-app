757677255a81c4f863beb9ce657ec669f4f5efc9
kyaunglanhtan <kyaunglanhtan98@gmail.com>
2026-03-30 15:23:32 +0630

    delete something

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,8 +10,6 @@ const Header = ({username = "user"} : HeaderProps) => {
   return (
     <div>
         <h2>React App</h2>
-        <h3>testing</h3>
-        <h4>testing again</h4>
         <p>username - {username} </p>
         <HeaderText text="this is from Header component"></HeaderText>
         <Button></Button>
